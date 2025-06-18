7609821f9ab7116ada3c1973e77c9eaa3d0c26f3
Levi <petercsontos29@gmail.com>
2025-06-18 23:14:46 +0200

    code style change

--- src/impl/x86_64/boot/main.64.asm
+++ src/impl/x86_64/boot/main.64.asm
@@ -5,13 +5,13 @@ section .text
 bits 64
 long_mode_start:
    ; load null into all data segment registers
-   mov ax, 0
-   mov ss, ax
-   mov ds, ax
-   mov es, ax
+	mov ax, 0
+	mov ss, ax
+	mov ds, ax
+	mov es, ax
 	mov fs, ax
-   mov gs, ax
+	mov gs, ax
 	
 	;calling odin func
 	call kernel_odin
-   hlt
+	hlt
